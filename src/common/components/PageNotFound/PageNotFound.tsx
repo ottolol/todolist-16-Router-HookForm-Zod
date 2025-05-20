1a332790dda174f98f5fec6e2663e945274f2ef8
Aleksey Sveshnikov <alexsveshnik@gmail.com>
2025-05-20 07:18:21 +0000

    5. Добавил кнопку Вернуться на главную

--- src/common/components/PageNotFound/PageNotFound.tsx
+++ src/common/components/PageNotFound/PageNotFound.tsx
@@ -1,8 +1,13 @@
 import styles from "./PageNotFound.module.css"
+import Button from '@mui/material/Button';
+import { Link } from 'react-router';
 
 export const PageNotFound = () => (
   <>
     <h1 className={styles.title}>404</h1>
     <h2 className={styles.subtitle}>page not found</h2>
+    <Button component={Link} to="/">
+      Вернуться на главную
+    </Button>
   </>
 )
